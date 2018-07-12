9a37e8ab8f437c3c0811e79ab2ddcd00ae0b9a88
Akash Manhas <akash.blogvault.net>
2018-07-12 12:27:36 +0530

    More Authentication in Enabling / Disabling the User OTP

--- app/controllers/switch_controller.rb
+++ app/controllers/switch_controller.rb
@@ -1,18 +1,28 @@
 class SwitchController < ApplicationController
 
+	before_action :authenticate_user!
+
 	def disable
-		@user = current_user
-		@user.otp_required_for_login = false
-		@user.otp_secret = ""
-		@user.save!
-		redirect_to root_path
+		@user=current_user
+		if @user.otp_required_for_login == true
+			@user.otp_required_for_login = false
+			@user.otp_secret = ""
+			@user.save!
+			redirect_to root_path
+		else
+			redirect_to root_path
+		end
 	end
 
 	def enable
-		@user = current_user
-		@user.otp_required_for_login = true
-		@user.otp_secret = User.generate_otp_secret
-		@user.save!
+		@user=current_user
+		if @user.otp_required_for_login == false
+			@user.otp_required_for_login = true
+			@user.otp_secret = User.generate_otp_secret
+			@user.save!
+		else
+			redirect_to root_path
+		end
 	end
 
 end
